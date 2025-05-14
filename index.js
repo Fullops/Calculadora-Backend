8d92671abdc17ba4efaa32dd7c9bced14a13f133
Sebastian Perdomo Cardozo <sebastianperdomocardozo@hotmail.com>
2025-05-14 07:54:29 -0500

    Add: Se añade la opeación de multiplicar

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ app.post('/subtract', (req, res) => {
   res.json({ result: a - b });
 });
 
+app.post('/multiply', (req, res) => {
+  const { a, b } = req.body;
+  res.json({ result: a * b });
+});
+
 app.listen(5000, () => {
   console.log('Calculadora API corriendo en http://localhost:5000');
 });
--- index.test.js
+++ index.test.js
@@ -5,6 +5,7 @@ app.use(express.json());
 
 app.post('/add', (req, res) => res.json({ result: req.body.a + req.body.b }));
 app.post('/subtract', (req, res) => res.json({ result: req.body.a - req.body.b }));
+app.post('/multiply', (req, res) => res.json({ result: req.body.a * req.body.b }));
 
 describe('Calculadora API', () => {
   it('suma correctamente', async () => {
@@ -26,4 +27,9 @@ describe('Calculadora API', () => {
     const res = await request(app).post('/subtract').send({ a: 8, b: 3 });
     expect(res.body.result).toBe(5);
   });
+
+  // it('multiplica correctamente', async () => {
+  //   const res = await request(app).post('/multiply').send({ a: 4, b: 5 });
+  //   expect(res.body.result).toBe(20);
+  // });
 });
